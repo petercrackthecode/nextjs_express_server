35c3d3ecacbd7e001e9affab6324f1b86186cbab
petercrackthecode <nguyenvandung02051999@gmail.com>
2022-03-05 16:04:08 -0800

    small changes

--- app/server.js
+++ app/server.js
@@ -1,26 +1,27 @@
-const express = require('express');
-const next = require('next')
+const express = require("express")
+const next = require("next")
 const NextjsExpressRouter = require("./nextjs_express_router")
 const Middleware = require("./middleware")
 
 const httpServer = (express) => {
-  return require('http').createServer(express)
+  return require("http").createServer(express)
 }
 
 const httpsServer = (express) => {
-  const fs = require('fs')
+  const fs = require("fs")
   const options = {
-    key: fs.readFileSync(process.env.SSL_PRIVATE_KEY_PATH, 'utf8'),
-    cert: fs.readFileSync(process.env.SSL_CERTIFICATE_PATH, 'utf8')
+    key: fs.readFileSync(process.env.SSL_PRIVATE_KEY_PATH, "utf8"),
+    cert: fs.readFileSync(process.env.SSL_CERTIFICATE_PATH, "utf8"),
   }
-  return require('https').createServer(options, express)
+
+  return require("https").createServer(options, express)
 }
 
 class Server {
   constructor(port) {
     this.port = port
     this.express = express()
-    this.next = next({ dev: process.env.NODE_ENV !== 'production' })
+    this.next = next({ dev: process.env.NODE_ENV !== "production" })
     this.middleware = new Middleware(this.express)
     this.router = new NextjsExpressRouter(this.express, this.next)
   }
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require("dotenv").config()
-const Server = require("./app/server")
+const server = require("./app/server")
 
 const begin = async () => {
-  await new Server(process.env.EXPRESS_PORT).start()
+  await new server(process.env.EXPRESS_PORT).start()
   console.log(`Server running in --- ${process.env.NODE_ENV} --- on port ${process.env.EXPRESS_PORT}`)
 }
 
